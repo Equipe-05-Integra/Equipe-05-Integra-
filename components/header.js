49eb44c6d3bcd6ec3a393b2919ed1481580215f0
Gabriel Conceição <gabrielconceicao2018@gmail.com>
2026-04-16 00:54:17 -0300

    Começando a linkagem de paginas

--- components/header.js
+++ components/header.js
@@ -8,11 +8,11 @@ class Header extends HTMLElement{
                 <img src="../img/Logo_Integra+.svg" alt="Logo_da_Integra_mais">
             </div>
             <nav class="links_container">
-                <a href="#">Inicio </a>
+                <a href="/index.html">Inicio </a>
                 <a href="#">Quem Somos </a>
                 <a href="#">Serviços </a>
                 <a href="#">Treinamentos </a>
-                <a href="#">Suporte </a>
+                <a href="/05_suporte.html">Suporte </a>
                 <a href="#">Noticias </a>
             </nav>
             <div class="container_botoes">
