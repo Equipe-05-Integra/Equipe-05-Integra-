70eb3f3a33b847bd506aad72937df1daaf999b89
Gabriel Conceição <gabrielconceicao2018@gmail.com>
2026-04-20 23:59:27 -0300

    Adicionando mais um link no menu lateral

--- components/header.js
+++ components/header.js
@@ -55,6 +55,7 @@ class Header extends HTMLElement{
                     <a href="/pages/04_treinamentos.html">Treinamentos</a>
                     <a href="/pages/05_suporte.html">Suporte</a>
                     <a href="/pages/noticias.html">noticias</a>
+                    <a href="/pages/07_diagnotico.html">Diagnosticar</a>
                 </nav>
             </aside>
             <div class="overlay" id="overlay"></div>
